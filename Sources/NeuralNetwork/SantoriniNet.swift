3314448259827c206744250158a5298b6b371498
Tomás Ruiz-López <truizlop@gmail.com>
2026-01-15 13:57:28 +0100

    Make network public

--- Sources/NeuralNetwork/SantoriniNet.swift
+++ Sources/NeuralNetwork/SantoriniNet.swift
@@ -12,14 +12,14 @@ import MLXNN
 // Hidden layers: 3 Linear layers with 256 neurons, ReLU
 // Output: policy head (153 logits = 25 for worker placements + 128 for worker-move-build directions)
 //         value head (tanh value -1...1 expected for outcome)
-class SantoriniNet: Module {
+public class SantoriniNet: Module {
     let layer1 = Linear(175, 256)
     let layer2 = Linear(256, 256)
     let layer3 = Linear(256, 256)
     let policyHead = Linear(256, 153)
     let valueHead = Linear(256, 1)
 
-    func callAsFunction(_ input: MLXArray) -> (policy: MLXArray, value: MLXArray) {
+    public func callAsFunction(_ input: MLXArray) -> (policy: MLXArray, value: MLXArray) {
         let o1 = relu(layer1(input))
         let o2 = relu(layer2(o1))
         let o3 = relu(layer3(o2))
