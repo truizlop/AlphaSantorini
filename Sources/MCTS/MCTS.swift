a242969d4e68e3573623e636f888fc1af2fabd86
Tomás Ruiz-López <truizlop@gmail.com>
2026-01-15 12:28:08 +0100

    Add MCTS algorithm

--- Sources/MCTS/MCTS.swift
+++ Sources/MCTS/MCTS.swift
@@ -1,3 +1,42 @@
-public struct MCTSConfig {
-    public init() {}
+//
+//  MCTS.swift
+//  AlphaSantorini
+//
+//  Created by Tomás Ruiz-López on 1/15/26.
+//
+
+public func mcts<State: GameState, Evaluator: PolicyValueNetwork>(
+    rootState: State,
+    evaluator: Evaluator,
+    iterations: Int,
+    explorationConstant: Float = 1.5
+) -> State.Move? where Evaluator.State == State {
+    let root = MCTSNode(
+        state: rootState,
+        move: nil,
+        prior: 0
+    )
+
+    root.expand(using: evaluator)
+
+    for _ in 0 ..< iterations {
+        var node = root
+
+        while node.isExpanded && !node.state.isTerminal {
+            guard let best = node.bestChild(explorationConstant: explorationConstant) else { break }
+            node = best
+        }
+
+        let value: Float = if node.state.isTerminal {
+            node.state.terminalValue
+        } else {
+            node.expand(using: evaluator)
+        }
+
+        node.backpropagate(value: value)
+    }
+
+    return root.children.max { a, b in
+        a.visits < b.visits
+    }?.move
 }
