7c4231ccd1221481acd0e3d7c6876320f9c3cf36
Tomás Ruiz-López <truizlop@gmail.com>
2026-01-15 11:52:30 +0100

    Define neural network for Santorini

--- Sources/NeuralNetwork/NeuralNetwork.swift
+++
@@ -1,9 +0,0 @@
-import MLX
-
-public struct NeuralNetworkModel {
-    public init() {}
-
-    public func exampleTensor() -> MLXArray {
-        MLXArray(0)
-    }
-}
---
+++ Sources/NeuralNetwork/SantoriniNet.swift
@@ -0,0 +1,30 @@
+//
+//  SantoriniNet.swift
+//  AlphaSantorini
+//
+//  Created by Tomás Ruiz-López on 1/15/26.
+//
+
+import MLX
+import MLXNN
+
+// Input: 175 values (one-hot encoding of 25 positions for 5 building levels, current player positions and other player positions)
+// Hidden layers: 3 Linear layers with 256 neurons, ReLU
+// Output: policy head (153 logits = 25 for worker placements + 128 for worker-move-build directions)
+//         value head (tanh value -1...1 expected for outcome)
+class SantoriniNet: Module {
+    let layer1 = Linear(175, 256)
+    let layer2 = Linear(256, 256)
+    let layer3 = Linear(256, 256)
+    let policyHead = Linear(256, 153)
+    let valueHead = Linear(256, 1)
+
+    func callAsFunction(_ input: MLXArray) -> (policy: MLXArray, value: MLXArray) {
+        let o1 = relu(layer1(input))
+        let o2 = relu(layer2(o1))
+        let o3 = relu(layer3(o2))
+        let policy = softmax(policyHead(o3))
+        let value = tanh(valueHead(o3))
+        return (policy: policy, value: value)
+    }
+}
