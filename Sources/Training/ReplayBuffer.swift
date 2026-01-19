cc444f4f7c4ea70428477cc28e301ccc8d74a782
Tomás Ruiz-López <truizlop@gmail.com>
2026-01-19 11:17:35 +0100

    Add replay buffer

---
+++ Sources/Training/ReplayBuffer.swift
@@ -0,0 +1,37 @@
+//
+//  ReplayBuffer.swift
+//  AlphaSantorini
+//
+//  Created by Tomás Ruiz-López on 1/16/26.
+//
+
+class ReplayBuffer {
+    private var samples: [TrainingSample] = []
+    private let maxSize: Int
+
+    init(maxSize: Int) {
+        self.maxSize = maxSize
+    }
+
+    func add(_ newSamples: [TrainingSample]) {
+        samples.append(contentsOf: newSamples)
+        if samples.count > maxSize {
+            samples.removeFirst(samples.count - maxSize)
+        }
+    }
+
+    func sample(batchSize: Int) -> [TrainingSample] {
+        Array(samples.shuffled().prefix(batchSize))
+    }
+
+    var count: Int {
+        samples.count
+    }
+
+    func checkDiversity() {
+        let hashes = samples.map { $0.state.encoded().description.hashValue }
+        let unique = Set(hashes).count
+
+        print("Diversity: \(String(format: "%.1f", Float(unique) / Float(samples.count)))")
+    }
+}
